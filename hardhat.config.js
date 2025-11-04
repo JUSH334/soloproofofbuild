eaa2a5a9e85e508cbf987344f88dfa21d28d079a
Josh Castro <joshuaecastro334@gmail.com>
2025-11-04 11:17:12 -0600

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+﻿require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    didlab: {
+      url: "https://eth.blockchain.didlab.org",
+      chainId: 252501,
+      accounts: ["0xcde553ec19806847ac8794493da93fb6549b937b815609c499a5faa6309a4cc8"],
+    },
+  },
+};

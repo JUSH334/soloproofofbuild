655bb09c981d5677129e48dc741af29c4ccaf47f
Josh Castro <122708086+JUSH334@users.noreply.github.com>
2025-11-04 23:40:27 -0600

    Update auth-siwe.js

--- auth-siwe.js
+++ auth-siwe.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const https = require("https");
 
 const ADDR = "0xFB3C61Dcc2dF6800C62E7ba2bcA5e9dd7d42f2F7";
-const PRIVKEY = "0xcde553ec19806847ac8794493da93fb6549b937b815609c499a5faa6309a4cc8";
+  const PRIVKEY = [PRIVATE_KEY];
 
 async function httpRequest(options, data = null) {
   return new Promise((resolve, reject) => {
